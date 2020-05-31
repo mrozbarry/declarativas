2070176cc9a9d874b5389cccfc71c446e5d47395
Alex Barry <alex.barry@gmail.com>
2020-05-31 17:27:30 -0400

    Simplify recursive render

--- src/lib/render.js
+++ src/lib/render.js
@@ -2,26 +2,33 @@ import { primitives } from './operations/index';
 
 export const render = (context, nodes) => {
   if (!nodes || nodes.length === 0) {
+    console.log('no nodes');
     return;
   }
 
-  const renderChildren = (childOperations) => render(
-    context,
-    childOperations,
-  );
-
   let node;
   let op;
-  for(node of nodes) {
+
+  for (node of nodes) {
+    if (!node) continue;
+
     if (Array.isArray(node)) {
       render(context, node);
       continue;
+    } else if (typeof node.type === 'function') {
+      render(context, node.type(node.props, node.children));
+      continue;
     }
-    op = primitives[node.type];
-    op.exec(
-      context,
-      node.props,
-      renderChildren,
-    );
+
+    try {
+      op = primitives[node.type];
+      op.exec(
+        context,
+        node.props,
+      );
+    } catch (exception) {
+      console.warn('Unable to render', node.value);
+      throw exception;
+    }
   }
 };
