0d565beaa1364f00bd3695fd9bafc52a1e9f2c64
Alex Barry <mrozbarry@users.noreply.github.com>
2020-06-02 12:17:43 -0400

    Next evolution of declarativas (#1)
    * Next evolution of declarativas
    
    * Simplify demo app
    
    * Remove ferp from dependencies

---
+++ src/demo/app.js
@@ -0,0 +1,25 @@
+import { render } from '../lib/index';
+
+function *makeIterator({
+  context,
+  view,
+}) {
+  let state = {};
+  let update = (nextState) => nextState;
+
+  while (true) {
+    update = yield state;
+    state = update({ ...state });
+    if (!state) return;
+    render(
+      context,
+      view(state, context),
+    );
+  }
+}
+
+export function make(props) {
+  const app = makeIterator(props);
+  app.next(); // iterators that both accept and publish data need this initial .next call
+  return app;
+}
--- src/demo/index.js
+++ src/demo/index.js
@@ -1,139 +1,112 @@
-import {
-  app,
-  c,
-  effects,
-} from '../lib/index';
+import { c } from '../lib/index';
+import * as app from './app';
+import * as actions from './actions';
+import * as rnd from './rnd';
 
 const toRadians = (degrees) => (
   degrees * Math.PI / 180
 );
 
-const Rotate = (state) => [
-  {
-    ...state,
-    squares: state.squares.map((square) => ({
-      ...square,
-      angle: square.angle + square.speed,
-    })),
-  },
-  effects.none(),
-];
-
-const stats = document.querySelector('#stats');
-const TrackFrames = (state) => {
-  const currentTime = performance.now();
-  const delta = currentTime - state.lastFrameAt;
-
-  stats.innerText = `
-FPS: ${(state.frameDeltas.length / (state.frameDeltas.reduce((sum, delta) => sum + delta, 0) / 1000)).toFixed(2)}
-`;
-
-  return [
-    {
-      ...state,
-      lastFrameAt: currentTime,
-      frameDeltas: [delta, ...state.frameDeltas.slice(0, 9)],
-    },
-    effects.none(),
-  ];
+const controls = {
+  numberOfRows: document.querySelector('input#numberOfRows'),
+  numberOfCols: document.querySelector('input#numberOfCols'),
+  fps: document.querySelector('#stats'),
 };
 
-const Initialize = ({ numberOfColumns, numberOfRows }) => (state) => [
-  {
-    ...state,
-    squares: Array.from({ length: numberOfColumns * numberOfRows}, (_, index) => ({
-      color: 'hsla(' + [
-        Math.random() * 359,
-        '60%',
-        '40%',
-        0.3,
-      ]
-        .join(', ') + ')',
-      angle: Math.random() * 360,
-      speed: (Math.random() * 6) - 3,
-      x: ((index % numberOfColumns) * (640 / numberOfColumns)) + (640 / numberOfColumns / 2),
-      y: (Math.floor(index / numberOfColumns) * (480 / numberOfRows)) + (480 / numberOfRows / 2),
-    })),
-  },
-  effects.none(),
-];
+const random = rnd.make();
 
-const ControlListeners = () => {
-  const rowsElement = document.querySelector('input#numberOfRows');
-  const colsElement = document.querySelector('input#numberOfCols');
+const pipe = (fns) => (value) => (
+  fns.reduce((state, fn) => fn(state), value)
+);
 
-  return (dispatch) => {
-    const trigger = () => {
-      dispatch(Initialize({
-        numberOfRows: rowsElement.value || 2,
-        numberOfColumns: colsElement.value || 2,
-      }));
-    };
-
-    const onInputChange = () => {
-      trigger();
-    };
-    rowsElement.addEventListener('input', onInputChange);
-    colsElement.addEventListener('input', onInputChange);
-
-    trigger();
-
-    return () => {
-      rowsElement.removeEventListener('input', onInputChange);
-      colsElement.removeEventListener('input', onInputChange);
-    };
-  };
-};
-
-const Group = (_props, children) => [
+const RevertableState = (_props, children) => [
   c('save'),
   children,
   c('restore'),
 ];
 
-app({
-  init: Initialize({
-    numberOfColumns: document.querySelector('input#numberOfCols').value,
-    numberOfRows: document.querySelector('input#numberOfRows').value,
-  })({
+const FilledRect = (props) => [
+  c('fillStyle', { value: props.fill }),
+  c('fillRect', {
+    x: props.x,
+    y: props.y,
+    width: props.width,
+    height: props.height,
+  }),
+];
+
+const Square = (props) => c(RevertableState, null, [
+  c('translate', {
+    x: props.x,
+    y: props.y,
+  }),
+
+  c('rotate', { value: toRadians(props.angle) }),
+
+  c(FilledRect, {
+    fill: props.color,
+    x: -35,
+    y: -35,
+    width: 70,
+    height: 70,
+  }),
+]);
+
+const game = app.make({
+  view: (state, context) => c(RevertableState, null, [
+    c(FilledRect, {
+      fill: '#d0d0d0',
+      x: 0,
+      y: 0,
+      width: context.canvas.width,
+      height: context.canvas.height,
+    }),
+
+    state.squares.map((square) => c(Square, square)),
+  ]),
+
+  context: document.querySelector('#app').getContext('2d'),
+});
+
+const loop = () => {
+  // Pure state functions
+  const state = game.next(pipe([
+    actions.Rotate,
+    actions.TrackFrames,
+  ])).value;
+
+  // Side effects here
+  controls.fps.innerText = `
+FPS: ${(state.frameDeltas.length / (state.frameDeltas.reduce((sum, delta) => sum + delta, 0) / 1000)).toFixed(2)}
+`;
+
+  requestAnimationFrame(loop);
+};
+
+const onControlInput = () => {
+  game.next(
+    actions.Initialize({
+      numberOfColumns: controls.numberOfCols.value || 2,
+      numberOfRows: controls.numberOfRows.value || 2,
+      rnd: random,
+    }),
+  );
+};
+controls.numberOfCols.addEventListener('input', onControlInput);
+controls.numberOfRows.addEventListener('input', onControlInput);
+
+game.next(pipe([
+  () => ({
+    play: true,
+    squares: [],
     lastFrameAt: performance.now(),
     frameDeltas: [],
   }),
+  actions.Initialize({
+    numberOfColumns: controls.numberOfCols.value,
+    numberOfRows: controls.numberOfRows.value,
+    rnd: random,
+  }),
+]));
 
-  render: (state, canvas) => [
-    c(Group, null, [
-      c('fillStyle', { value: '#d0d0d0' }),
-      c('fillRect', {
-        x: 0,
-        y: 0,
-        width: canvas.width,
-        height: canvas.height,
-      }),
-
-      state.squares.map((square) => c(Group, null, [
-        c('translate', {
-          x: square.x,
-          y: square.y,
-        }),
-
-        c('rotate', { value: toRadians(square.angle) }),
-
-        c('fillStyle', { value: square.color }),
-        c('fillRect', {
-          x: -35,
-          y: -35,
-          width: 70,
-          height: 70,
-        }),
-      ])),
-    ])
-  ],
-
-  subscribe: (state, builtIns) => [
-    [builtIns.onFrame, Rotate],
-    [builtIns.onFrame, TrackFrames],
-    [ControlListeners],
-  ],
-
-  canvas: document.querySelector('canvas#app'),
-});
+loop();
