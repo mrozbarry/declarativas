62cb944b91c27209ac706bec2ec1ef7dd1fb9ad2
Alex Barry <alex.barry@gmail.com>
2020-06-05 13:04:34 -0400

    Build support

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import { eslint } from 'rollup-plugin-eslint';
+import { terser } from 'rollup-plugin-terser';
+import strip from '@rollup/plugin-strip';
+import cleanup from 'rollup-plugin-cleanup';
+
+export default {
+  input: './src/lib/index.js',
+  plugins: [
+    eslint(),
+    terser(),
+    strip(),
+    cleanup(),
+  ],
+  output: [
+    {
+      name: 'declarativas',
+      file: 'declarativas.umd.js',
+      format: 'umd',
+      exports: 'named',
+    },
+    {
+      file: 'declarativas.esm.js',
+      format: 'esm',
+    },
+  ],
+};
