98075bb2b7e1532374deaad357b9d77411252b6d
Alex Barry <alex.barry@gmail.com>
2020-08-12 09:15:02 -0400

    Ellipse component

---
+++ src/lib/components/ellipse.js
@@ -0,0 +1,31 @@
+import { c } from '../c';
+
+export const ellipse = (props) => [
+  props.fill && [
+    c('fillStyle', { value: props.fill }),
+    c('beginPath'),
+    c('ellipse', {
+      x: props.cx,
+      y: props.cy,
+      radiusX: props.rx,
+      radiusY: props.ry,
+      startAngle: 0,
+      endAngle: props.pathLength || Math.PI * 2,
+    }),
+    c('fill'),
+  ],
+  props.stroke && [
+    c('strokeStyle', { value: props.stroke }),
+    c('beginPath'),
+    c('ellipse', {
+      x: props.cx,
+      y: props.cy,
+      radiusX: props.rx,
+      radiusY: props.ry,
+      startAngle: 0,
+      endAngle: props.pathLength || Math.PI * 2,
+    }),
+    c('stroke'),
+  ],
+];
+
